96c278043dc28e2d2d96d37418d18da26f9a1c3e
haolipeng <1078285863@qq.com>
2021-12-08 23:10:56 +0800

    今天加班到家11点了，微习惯不能断，抄代码也要起码抄十行

--- GeeGrpc/day1-codec/codec/codec.go
+++ GeeGrpc/day1-codec/codec/codec.go
@@ -16,3 +16,17 @@ type Codec interface {
 }
 
 type NewCodecFunc func(closer io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json" //not implemented
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	//NewCodecFuncMap[GobType] = NewCodecFunc
+}
