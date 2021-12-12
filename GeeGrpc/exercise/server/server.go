4cc6cdeafbecca602d672805ad33a4561b7254c4
haolipeng <1078285863@qq.com>
2021-12-12 22:32:27 +0800

    熟悉golang远程rpc库的使用（1）

---
+++ GeeGrpc/exercise/server/server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quetient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (a *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quetient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+
+	return nil
+}
+
+func main() {
+	//注册rpc对象
+	arith := new(Arith)
+	rpc.Register(arith)
+
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("listen error:", e)
+	}
+
+	go http.Serve(l, nil)
+}
